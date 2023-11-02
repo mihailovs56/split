19dddbff0b8026c52bc902229a47ba2cee27397f
Михайлов <@DESKTOP-84VD5MI>
2023-11-02 10:50:53 +0500

    Добавьте файлы проекта.

---
+++ Практика7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строчку с символами: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите этот символ для разделения: ");
+            char b = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            string[] c = a.Split(b);
+
+            Console.WriteLine("Результат разделения:");
+            foreach (string n in c)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
